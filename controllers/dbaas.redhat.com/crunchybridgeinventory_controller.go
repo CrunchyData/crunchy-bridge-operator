0ac40443f950b8f04c84fe2e60dd0e5500dd736a
Abdul Hameed <ahameed@redhat.com>
2021-06-09 12:34:50 -0400

    updated inventory controller
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

--- controllers/dbaas.redhat.com/crunchybridgeinventory_controller.go
+++ controllers/dbaas.redhat.com/crunchybridgeinventory_controller.go
@@ -18,9 +18,11 @@ package dbaasredhatcom
 
 import (
 	"context"
+	"fmt"
 	"github.com/CrunchyData/crunchy-bridge-operator/controllers/resources"
-
+	dbaasv1alpha1 "github.com/RHEcosystemAppEng/dbaas-operator/api/v1alpha1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -71,8 +73,24 @@ func (r *CrunchyBridgeInventoryReconciler) Reconcile(ctx context.Context, req ct
 		logger.Error(err, "error fetching matching Secret")
 		return ctrl.Result{}, err
 	}
-	//TODO need to remove from logs once utiliaze the connectionAPIKeys
-	logger.Info("CrunchyBridgeInventory ", "connectionAPIKeys", connectionAPIKeys)
+
+	err = r.updateStatus(&inventory, connectionAPIKeys)
+	if err != nil {
+		logger.Error(err, "error fetching instances status")
+		return ctrl.Result{}, err
+	}
+	UpdateCondition(&inventory, "crunchybridgeinventory", metav1.ConditionTrue, "Created")
+	if err = r.Status().Update(ctx, &inventory); err != nil {
+		if apierrors.IsConflict(err) {
+			logger.Info("conflict at status update, requeue to sort it out")
+			return ctrl.Result{}, nil
+		} else {
+			logger.Error(err, "error saving modified CrunchyBridgeInventory status")
+
+			return ctrl.Result{}, err
+		}
+	}
+
 	return ctrl.Result{}, nil
 }
 
@@ -81,4 +99,59 @@ func (r *CrunchyBridgeInventoryReconciler) SetupWithManager(mgr ctrl.Manager) er
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&dbaasredhatcomv1alpha1.CrunchyBridgeInventory{}).
 		Complete(r)
+}
+
+func (r *CrunchyBridgeInventoryReconciler) updateStatus(dbaasredhatcomv1alpha1 *dbaasredhatcomv1alpha1.CrunchyBridgeInventory, keys resources.ConnectionAPIKeys) error {
+	var bridgeInstances []dbaasv1alpha1.Instance
+	instance := dbaasv1alpha1.Instance{
+		InstanceID:   "azpiatrcn5eujmhncap73ujeqm",
+		Name:         "example-cluster",
+		InstanceInfo: map[string]string{"provider_id": "aws", "region_id": "us-east-1", "type": "primary"},
+	}
+
+	bridgeInstances = append(bridgeInstances, instance)
+	dbaasredhatcomv1alpha1.Status.Instances = bridgeInstances
+	dbaasredhatcomv1alpha1.Status.Type = "CrunchyBridge Postgres"
+	return nil
+}
+
+// UpdateCondition will update or add the provided condition.
+func UpdateCondition(cr *dbaasredhatcomv1alpha1.CrunchyBridgeInventory, conditionType string, status metav1.ConditionStatus, reason string) {
+
+	message := fmt.Sprintf("%s inventory listed successfully", cr.Name)
+	found := false
+
+	condition := &metav1.Condition{
+		Type:               conditionType,
+		Status:             status,
+		Reason:             reason,
+		Message:            message,
+		LastTransitionTime: metav1.Now(),
+	}
+	conditions := []metav1.Condition{}
+
+	for _, c := range cr.Status.Conditions {
+		if condition.Type != c.Type {
+			conditions = append(conditions, c)
+			continue
+		}
+		if c.Status != condition.Status {
+			c.Status = condition.Status
+			c.LastTransitionTime = condition.LastTransitionTime
+		}
+		if c.Reason != condition.Reason {
+			c.Reason = condition.Reason
+		}
+		if c.Message != condition.Message {
+			c.Message = condition.Message
+		}
+		conditions = append(conditions, c)
+		found = true
+	}
+	cr.Status.Conditions = conditions
+	if !found {
+		conditions = append(conditions, *condition)
+	}
+	cr.Status.Conditions = conditions
+
 }
